af3a12376f3692965609185b005ccd8d7a474d42
jazcash <jaspercashmore@gmail.com>
2017-12-05 00:29:27 +0000

    added sc2 stuff

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+const config = require("../config");
+
 import * as Discord from "discord.js";
 
 const jazbot = new Discord.Client();
 
 jazbot.on("message", message => {
-	if (message.content === "!boop"){
+	if (message.content === "!boop") {
 		var voiceChannel = message.member.voiceChannel;
 		voiceChannel.join().then(connection => {
 			const dispatcher = connection.playFile("../sounds/Alert_ZergNeedMoreFood.mp3");
@@ -18,18 +20,18 @@ jazbot.on("message", message => {
 
 if (process.platform === "win32") {
 	var rl = require("readline").createInterface({
-	  input: process.stdin,
-	  output: process.stdout
+		input: process.stdin,
+		output: process.stdout
 	});
 
 	rl.on("SIGINT", function () {
-	  process.emit("SIGINT");
+		process.emit("SIGINT");
 	});
-  }
+}
 
-  process.on("SIGINT", function () {
+process.on("SIGINT", function () {
 	//graceful shutdown
 	process.exit();
-  });
+});
 
-jazbot.login("MjQ1Mjg1OTQ4MDA1Mjg1ODg5.DQcIuQ.x6TXog9KpJP_288FkToK6zk7qbU");
+jazbot.login(config.key);
